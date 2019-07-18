830ecc9fc4d3d43ec3ed7cbf7af6108e71e252f3
leonard <sirerr@gmail.com>
2019-07-17 23:21:47 -0400

    need to get basics working

---
+++ Assets/Scripts/BaseInteractive.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BaseInteractive : MonoBehaviour
+{
+    public enum GameParts { building, character};
+    public GameParts GamePart;
+    public bool Busy = false;
+
+
+ protected virtual void ObjMessage()
+    {
+
+    }
+
+}
---
+++ Assets/Scripts/InputControlSystem.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InputControlSystem : MonoBehaviour
+{
+    public static InputControlSystem inputsystem;
+    Vector2 mousePos;
+    Ray CamRay;
+    //current object selected
+  public  GameObject CurrentObject;
+
+    private void Awake()
+    {
+        inputsystem = this;
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+
+        if(Input.GetMouseButtonDown(0))
+        {
+            SelectObj();
+        }
+    }
+
+    void SelectObj()
+    {
+        RaycastHit rayhit;
+        mousePos = Input.mousePosition;
+        CamRay = Camera.main.ScreenPointToRay(mousePos);
+
+        if (Physics.Raycast(CamRay, out rayhit, Mathf.Infinity))
+        {
+            CurrentObject = rayhit.transform.gameObject;
+            print(rayhit.transform.tag);
+        }
+    }
+}
