3a6e857643eed2cd703c20838e5fac8be11656ce
leonard <sirerr@gmail.com>
2019-10-03 00:07:54 -0400

    getting it up to date

---
+++ Assets/Scripts/BasicCharMovement.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BasicCharMovement : MonoBehaviour
+{
+    public float speed;
+    public Animator anim;
+
+    Rigidbody rbody;
+
+    void Awake()
+    {
+        anim = GetComponent<Animator>();
+        rbody = GetComponent<Rigidbody>();
+    }
+    
+    public void FixedUpdate()
+    {
+        float hor = Input.GetAxis("Horizontal");
+        float ver = Input.GetAxis("Vertical");
+
+        anim.SetFloat("mover", ver);
+        if (Input.GetButtonDown("Jump"))
+            anim.SetTrigger("jump");
+    }
+}
